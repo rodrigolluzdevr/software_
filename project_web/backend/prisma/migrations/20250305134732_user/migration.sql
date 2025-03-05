9c5d912ca0bf2c6eed04c802104a0b8ea1d90c84
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-03-05 10:49:05 -0300

    refactor: new table in database.

--- project_web/backend/prisma/migrations/20250305134732_user/migration.sql
+++ project_web/backend/prisma/migrations/20250305134732_user/migration.sql
@@ -1,13 +1,13 @@
 -- CreateTable
-CREATE TABLE "Users" (
+CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "role" TEXT NOT NULL DEFAULT 'USER',
 
-    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
