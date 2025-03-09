fd31e6f549f004557543518b7ef8a39e7ae93c0a
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-03-09 15:26:32 -0300

    refactor: refactor in code project

--- project_web/backend/src/app.module.ts
+++ project_web/backend/src/app.module.ts
@@ -3,22 +3,15 @@ import { PrismaService } from './database/prisma.service';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import * as dotenv from 'dotenv'; // Importando dotenv
-import { DashboardController } from './modules/dashboard/dashboard.controller';
 
 // Carregar variáveis do .env
 dotenv.config();
 
 @Module({
   imports: [
-    UserModule,
-    AuthModule,
+    UserModule,  // Módulo para gerenciar usuários
+    AuthModule,  // Módulo para gerenciar autenticação
   ],
-  controllers: [DashboardController],
-  providers: [PrismaService],
+  providers: [PrismaService],  // Serviço para conectar ao banco de dados
 })
-export class AppModule {
-  constructor() {
-    // Verificando se a variável está sendo lida corretamente
-    console.log('JWT_SECRET from .env:', process.env.JWT_SECRET);
-  }
-}
+export class AppModule {}
