c896f6a911a391a269ace7c87470a85ec2d8700c
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-03-09 16:01:54 -0300

    start

--- project_web/backend/src/main.ts
+++ project_web/backend/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+
+dotenv.config({ path: '../infra/.env' });
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Habilitar CORS para localhost:3000
   app.enableCors({
-    origin: 'http://localhost:3000', // Permite requisições apenas do frontend na porta 3000
+    origin: 'http://localhost:3000',
   });
 
   await app.listen(4000);
