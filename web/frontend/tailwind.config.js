0115dd6628f56603337f416f96daed7ffec24c3d
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-04-09 20:11:02 -0300

    feat: add Tailwind CSS configuration and VSCode settings for improved styling support

---
+++ web/frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      nunito: 'var(--font-nunito)',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
